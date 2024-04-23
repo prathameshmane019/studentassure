b35160618db077b64a1ad570775f63baf5af72ea
12DeshmukhAish <113546938+12DeshmukhAish@users.noreply.github.com>
2024-04-23 23:40:09 +0530

    admin part added

---
+++ app/(main)/admin/feedbacks/page.js
@@ -0,0 +1,12 @@
+import FeedbackForm from '@/components/feedbackfrom'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='h-screen overflow-y-auto'>
+      <FeedbackForm/>
+    </div>
+  )
+}
+
+export default page
